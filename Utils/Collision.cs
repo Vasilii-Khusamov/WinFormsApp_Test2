e246a291510229fe3b80a1ff919812dbf04d1c6d
Khusamov Sukhrob <khusamov@yandex.ru>
2022-08-06 21:49:46 +0300

    Добавлена функция проверки столкновения падающей фигуры с кирпичиками

--- Utils/Collision.cs
+++ Utils/Collision.cs
@@ -32,5 +32,36 @@
 
 			return result;
 		}
+
+		/// <summary>
+		/// Проверка на столкновение падающей фигурки с кирпичиками стакана.
+		/// </summary>
+		/// <param name="gameState">Ссылка на состояние игры.</param>
+		/// <returns>Возвращает true, если фигурка пересекается с кирпичиками стакана.</returns>
+		static public bool CheckFallingShapeCupCollision(GameState gameState)
+		{
+			bool result = false;
+			for (int col = 0; col < gameState.FallingShapeCols; col++)
+			{
+				for (int row = 0; row < gameState.FallingShapeRows; row++)
+				{
+					if (
+						gameState.FallingShape[row, col] != BrickBrushPalette.EmptyBrickBrush
+						&& col + gameState.FallingShapeCol >= 0 && col + gameState.FallingShapeCol < gameState.CupCols
+						&& row + gameState.FallingShapeRow >= 0 && row + gameState.FallingShapeRow < gameState.CupRows
+					)
+					{
+						if (gameState.Cup[row + gameState.FallingShapeRow, col + gameState.FallingShapeCol] != BrickBrushPalette.EmptyBrickBrush)
+						{
+							result = true;
+							break;
+						}
+					}
+				}
+				if (result == true) break;
+			}
+
+			return result;
+		}
 	}
 }
