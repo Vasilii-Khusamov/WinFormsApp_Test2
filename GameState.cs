334f1ac36cc0396068e77740ef33a0285e756241
Khusamov Sukhrob <khusamov@yandex.ru>
2022-08-06 16:28:36 +0300

    Добавлены размеры стакана

--- GameState.cs
+++ GameState.cs
@@ -1,8 +1,11 @@
-﻿namespace WinFormsApp_Test2
+namespace WinFormsApp_Test2
 {
 	internal class GameState
 	{
 		public Brush[,] Cup { get; set; } = new Brush[0, 0];
+		public int CupCols => Cup.GetUpperBound(1) + 1;
+		public int CupRows => Cup.GetUpperBound(0) + 1;
+
 		public int Score { get; set; }
 		public string State { get; set; } = "pause";
 
