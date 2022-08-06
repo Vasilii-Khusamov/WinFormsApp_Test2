d8b080342358a7824ee316eed917a068e50dc6a4
Khusamov Sukhrob <khusamov@yandex.ru>
2022-08-06 21:56:20 +0300

    Добавлена функция объединения массивов

---
+++ Utils/BrickMapOperations.cs
@@ -0,0 +1,32 @@
+﻿namespace WinFormsApp_Test2.Utils
+{
+	internal class BrickMapOperations
+	{
+		/// <summary>
+		/// Объединение массивов из target в destination со смещением.
+		/// Пустые ячейки не переносятся.
+		/// </summary>
+		/// <param name="destination"></param>
+		/// <param name="target"></param>
+		/// <param name="offsetCol">Смещение по х</param>
+		/// <param name="offsetRow">Смещение по у</param>
+		/// <returns>Объединенный массив</returns>
+		public static Brush[,] Merge(Brush[,] destination, Brush[,] target, int offsetCol, int offsetRow)
+		{
+			Brush[,] result = (Brush[,])destination.Clone();
+
+			for (int col = 0; col < target.GetUpperBound(1) + 1; col++)
+			{
+				for (int row = 0; row < target.GetUpperBound(0) + 1; row++)
+				{
+					if (target[row, col] != BrickBrushPalette.EmptyBrickBrush)
+					{
+						result[row + offsetRow, col + offsetCol] = target[row, col];
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
