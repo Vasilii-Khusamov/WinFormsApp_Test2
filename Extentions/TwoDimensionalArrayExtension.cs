7439244d19974e908cf5e1abbcba828cfabde270
Khusamov Sukhrob <khusamov@yandex.ru>
2022-08-09 14:19:46 +0300

    Добавлен метод вычисления размера массива

---
+++ Extentions/TwoDimensionalArrayExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp_Test2.Extentions
+{
+	internal static class TwoDimensionalArrayExtension
+	{
+		public static (int, int) GetSize<T>(this T[,] array)
+		{
+			int rows = array.GetUpperBound(0) + 1;
+			int columns = array.Length / rows;
+			return (rows, columns);
+		}
+	}
+}
