9316873909c5763d89adf9648465d27bf2eaa9d5
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2022-08-10 20:31:16 +0300

    Исправлена команда вращения фигуры

--- Commands/RotateFallingShape.cs
+++ Commands/RotateFallingShape.cs
@@ -9,9 +9,48 @@ namespace WinFormsApp_Test2.Commands
 {
 	internal class RotateFallingShape : Command
 	{
+		/// <summary>
+		/// Эта команда вращает фигуру.
+		/// 
+		/// Если при вращении фигуры она пересекается со стаканом или кирпичиками, тов
+		/// команда попытается сдвинуть фигуру вправо или влево на один кирпичик.
+		/// Если пересечение осталось, то вращение отменяется.
+		/// </summary>
+		/// <param name="gameState"></param>
 		public override void Execute(GameState gameState)
 		{
-			gameState.FallingShape = BrickMapOperations.Rotate(gameState.FallingShape);
+			GameState gameStateClone = gameState.Clone();
+
+			bool thereIsNoPlace = false;
+
+            Func<bool> isCollision = () => 
+			(
+				Utils.Collision.CheckFallingShapeWallsCollision(gameStateClone) 
+				|| Utils.Collision.CheckFallingShapeCupCollision(gameStateClone)
+			);
+
+			gameStateClone.FallingShape = BrickMapOperations.Rotate(gameStateClone.FallingShape);
+
+			if (isCollision())
+            {
+				gameStateClone.FallingShapeCol += 1;
+
+				if (isCollision())
+                {
+					gameStateClone.FallingShapeCol -= 2;
+
+					if (isCollision())
+                    {
+						thereIsNoPlace = true;
+                    }
+                }
+            }
+
+            if (!thereIsNoPlace)
+            {
+				gameState.FallingShape = BrickMapOperations.Rotate(gameState.FallingShape);
+				gameState.FallingShapeCol = gameStateClone.FallingShapeCol;
+			}
 		}
 	}
 }
