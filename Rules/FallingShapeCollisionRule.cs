43efec1203dfe3eff6b7ea11ed4ec33fe00a8bba
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2022-08-14 12:57:15 +0300

    добавил возможность скользить фигурой по полу

--- Rules/FallingShapeCollisionRule.cs
+++ Rules/FallingShapeCollisionRule.cs
@@ -1,4 +1,4 @@
-using WinFormsApp_Test2.Utils;
+﻿using WinFormsApp_Test2.Utils;
 
 namespace WinFormsApp_Test2.Rules
 {
@@ -6,7 +6,6 @@ namespace WinFormsApp_Test2.Rules
 	{
 		public override void Start()
 		{
-
 		}
 
 		public override void Tick(GameState gameState)
@@ -15,7 +14,14 @@ namespace WinFormsApp_Test2.Rules
 
 			gameStateClone.FallingShapeRow++;
 
-			if (Collision.CheckFallingShapeWallsCollision(gameStateClone) || Collision.CheckFallingShapeCupCollision(gameStateClone))
+			if 
+			(
+				(
+					Collision.CheckFallingShapeWallsCollision(gameStateClone) 
+					|| Collision.CheckFallingShapeCupCollision(gameStateClone)
+				)
+				&& gameState.FallingShapeSlideTimer < 1
+			)
 			{
 				// Копирование падающей фигуры.
 				gameState.Cup = BrickMapOperations.Merge(gameState.Cup, gameState.FallingShape, gameState.FallingShapeCol, gameState.FallingShapeRow);
@@ -24,6 +30,27 @@ namespace WinFormsApp_Test2.Rules
 				gameState.FallingShapeCol = 3;
 				gameState.FallingShapeRow = -2;
 				gameState.FallingShapeTimer = GameState.FallingShapeDelay;
+				gameState.FallingShapeSlideTimer = GameState.FallingShapeSlideDelay;
+			}
+
+			if 
+			(
+				gameState.FallingShapeSlideTimer < 1
+				&&
+				(
+					Collision.CheckFallingShapeWallsCollision(gameStateClone)
+					|| Collision.CheckFallingShapeCupCollision(gameStateClone)
+				)
+			)
+            {
+				gameState.FallingShapeSlideTimer = GameState.FallingShapeSlideDelay;
+			}
+			else if (Collision.CheckFallingShapeWallsCollision(gameStateClone) || Collision.CheckFallingShapeCupCollision(gameStateClone))
+			{
+				gameState.FallingShapeSlideTimer -= 1;
+			}
+			else if (gameState.FallingShapeSlideTimer < 1)
+			{
 				gameState.FallingShapeSlideTimer = GameState.FallingShapeSlideDelay;
 			}
 		}
