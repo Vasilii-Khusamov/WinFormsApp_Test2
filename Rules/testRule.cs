153148e8f718794fbf2e022c9fe118a48e4c848f
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2022-08-15 17:37:07 +0300

    Создано тестовое правило

---
+++ Rules/testRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp_Test2.Rules
+{
+    internal class TestRule : Rule
+    {
+        public override void Start()
+        {
+
+        }
+
+        public override void Tick(GameState gameState)
+        {
+            // Тестовое правило.
+        }
+    }
+}
